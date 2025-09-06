1e040bc63c3f64e16dd0566c7eea990003de33a4
bel1ch1 <andreyzv5555@gmail.com>
2025-09-06 10:49:24 +0300

    app config

---
+++ src/config/config.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional
+
+class ConfigBase(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="C:/work/Dota-AI-Assistant/.env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+class TelegramConfig(ConfigBase):
+    model_config = SettingsConfigDict(env_prefix="tg_")
+
+    bot_token: str
+
+class AiConfig(ConfigBase):
+    model_config = SettingsConfigDict(env_prefix="ai_")
+
+    openai_api_base: str
+    openai_api_key: str
+    model_name: str
+    embedder: str
+
+
+tg_congig = TelegramConfig()
+ai_config = AiConfig()
