2da539a742e7a0cfe36a018d9af54c36116db819
bel1ch1 <andreyzv5555@gmail.com>
2025-09-08 16:53:34 +0300

    changing the structure and removing garbage

--- src/ai/pipelines/data_pipe/steps/config_parsing_step.py
+++
@@ -1,8 +0,0 @@
-from zenml import step
-from utils.config_parser import LinkSorter
-
-@step
-def config_parsing_step(input_yaml_file: str):
-    extractor = LinkSorter()
-    extractor.parse_yaml(file_path=input_yaml_file)
-    return extractor.get_result()
